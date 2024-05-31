c3e5031ee0b4e4d701d7a9d5f0bf820db09f3bbb
RavenGoku <raven.goku@gmail.com>
2024-05-31 01:11:46 +0100

    number and simple math function and enumeration

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -17,6 +17,7 @@ namespace WPF_CalculatorApp
     public partial class MainWindow : Window
     {
         private double _lastNumber, _result;
+        private SelectedOperator SelectedOperator;
 
         public MainWindow()
         {
@@ -29,14 +30,40 @@ namespace WPF_CalculatorApp
 
         private void EqualButton_Click(object sender, RoutedEventArgs e)
         {
+            double newNumber;
+            if (double.TryParse(resultLabel.Content.ToString(), out newNumber))
+            {
+                switch (SelectedOperator)
+                {
+                    case SelectedOperator.Addition:
+                        _result = SimpleMath.Add(_lastNumber, newNumber);
+                        break;
+
+                    case SelectedOperator.Subtraction:
+                        _result = SimpleMath.Subtraction(_lastNumber, newNumber);
+                        break;
+
+                    case SelectedOperator.Multiplication:
+                        _result = SimpleMath.Multiply(_lastNumber, newNumber);
+                        break;
+
+                    case SelectedOperator.Division:
+                        _result = SimpleMath.Divide(_lastNumber, newNumber);
+                        break;
+                }
+                resultLabel.Content = _result.ToString();
+            }
         }
 
         private void PercentageButton_Click(object sender, RoutedEventArgs e)
         {
-            if (double.TryParse(resultLabel.Content.ToString(), out _lastNumber))
+            double tempNumber;
+            if (double.TryParse(resultLabel.Content.ToString(), out tempNumber))
             {
-                _lastNumber = _lastNumber / 100;
-                resultLabel.Content = _lastNumber.ToString();
+                tempNumber = (tempNumber / 100);
+                if (_lastNumber != 0)
+                    tempNumber *= _lastNumber;
+                resultLabel.Content = tempNumber.ToString();
             }
         }
 
@@ -57,12 +84,41 @@ namespace WPF_CalculatorApp
         private void OperationButtonClicked(object sender, RoutedEventArgs e)
         {
             Button OperButton = (Button)sender;
+            if (double.TryParse(resultLabel.Content.ToString(), out _lastNumber))
+            {
+                resultLabel.Content = "0";
+            }
+            if (OperButton == multiplicationButton)
+            {
+                SelectedOperator = SelectedOperator.Multiplication;
+            }
+            if (OperButton == divisionButton)
+            {
+                SelectedOperator = SelectedOperator.Division;
+            }
+            if (OperButton == plusButton)
+            {
+                SelectedOperator = SelectedOperator.Addition;
+            }
+            if (OperButton == minusButton)
+            {
+                SelectedOperator = SelectedOperator.Subtraction;
+            }
+        }
+
+        private void pointButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (!resultLabel.Content.ToString().Contains("."))
+            {
+                resultLabel.Content = $"{resultLabel.Content}.";
+            }
         }
 
         private void NumberButtonClicked(object sender, RoutedEventArgs e)
         {
             //create Button class and assign sender as Button by casting
             Button NumButton = (Button)sender;
+            //int selectedValue = int.Parse((sender as Button).Content.ToString());
 
             if (resultLabel.Content.ToString() == "0")
             {
@@ -74,6 +130,39 @@ namespace WPF_CalculatorApp
                 //use button object.content and assign to label content and concatination with content already in label
                 resultLabel.Content = $"{resultLabel.Content}{NumButton.Content}";
             }
+        }
+    }
+
+    //create enum Operation
+    public enum SelectedOperator
+    { Addition, Subtraction, Multiplication, Division };
+
+    //create new class SimpleMath
+    public class SimpleMath
+    {
+        public static double Add(double num1, double num2)
+        {
+            return num1 + num2;
+        }
+
+        public static double Subtraction(double num1, double num2)
+        {
+            return num1 - num2;
+        }
+
+        public static double Multiply(double num1, double num2)
+        {
+            return num1 * num2;
+        }
+
+        public static double Divide(double num1, double num2)
+        {
+            if (num2 == 0)
+            {
+                MessageBox.Show("Can't divide by 0", "Wrong Operation", MessageBoxButton.OK, MessageBoxImage.Error);
+                return 0;
+            }
+            return (num1 / num2);
         }
     }
 }
